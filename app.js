30ba282dc0ec8c5aa440c3df0170c0819207b074
jefferyhaupt <43371332+jefferyhaupt@users.noreply.github.com>
2019-10-11 19:51:48 -0400

    started project

---
+++ app.js
@@ -0,0 +1,22 @@
+let myName = "Jeff Haupt";
+
+const numStates = 50;
+
+let result = 5+4;
+
+function sayHello() {
+  alert("Hello World!");
+}
+
+sayHello();
+
+function checkAge(name, age) {
+  if (age < 21) {
+    alert("Sorry " + name + ", you are not old enough to view this page.");
+  }
+}
+
+checkAge("Charles", 21);
+checkAge("Abbey", 27);
+checkAge("James", 18);
+checkAge("John", 17);
