52fcde93956878cac75d7b1a3f13c5c9f46f5d5d
jefferyhaupt <43371332+jefferyhaupt@users.noreply.github.com>
2019-10-11 20:40:10 -0400

    Finish rest of assignment

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
+// create myName variable
 let myName = "Jeff Haupt";
 
+// create constant variable for number of states
 const numStates = 50;
 
+// create variable that stores sum of 5+4
 let result = 5+4;
 
+//create and call sayHello function
 function sayHello() {
   alert("Hello World!");
 }
 
 sayHello();
 
+
+//create checkAge function and call it four times
 function checkAge(name, age) {
   if (age < 21) {
     alert("Sorry " + name + ", you are not old enough to view this page.");
@@ -21,12 +27,15 @@ checkAge("Abbey", 27);
 checkAge("James", 18);
 checkAge("John", 17);
 
+
+//Create array of vegetables and console.log each one
 let vegetables = ["carrots", "broccoli", "corn", "green beans", "asparagus"];
 
 for (let i = 0; i < vegetables.length; i++) {
   console.log(vegetables[i]);
 }
 
+//Number 10 - create array of objects and call checkAge
 let peopleInfo = [
   {name: "Jeff", age: 44},
   {name: "Jada", age: 14},
@@ -37,4 +46,20 @@ let peopleInfo = [
 
 for(let i = 0; i < peopleInfo.length; i++){
   checkAge(peopleInfo[i].name, peopleInfo[i].age);
+}
+
+
+//Number 11 - create getLength, call and assign to variable, check to see if
+//it is an odd or even number of characters
+function getLength(word){
+  let howManyCharacters = word.length;
+  return howManyCharacters;
+}
+
+let wordLength = getLength("Hello World");
+
+if ((wordLength % 2) == 0) {
+  console.log("The world is nice and even!");
+} else {
+  console.log("The world is an odd place!");
 }
