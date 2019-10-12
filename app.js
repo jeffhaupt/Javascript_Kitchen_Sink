1636eb7c40884be00062561f70c2518e6c3f19a1
jefferyhaupt <43371332+jefferyhaupt@users.noreply.github.com>
2019-10-11 20:23:57 -0400

    Finished arrays and an array of objects

--- app.js
+++ app.js
@@ -20,3 +20,21 @@ checkAge("Charles", 21);
 checkAge("Abbey", 27);
 checkAge("James", 18);
 checkAge("John", 17);
+
+let vegetables = ["carrots", "broccoli", "corn", "green beans", "asparagus"];
+
+for (let i = 0; i < vegetables.length; i++) {
+  console.log(vegetables[i]);
+}
+
+let peopleInfo = [
+  {name: "Jeff", age: 44},
+  {name: "Jada", age: 14},
+  {name: "Lindsay", age: 36},
+  {name: "Cali", age: 1},
+  {name: "Zeus", age: 7}
+]
+
+for(let i = 0; i < peopleInfo.length; i++){
+  checkAge(peopleInfo[i].name, peopleInfo[i].age);
+}
